2d0ce10db4d7b0a0fa39146d0bfb450ce3e72b57
ceciliaknaub <ceciliaknaub@client-104-39-87-43.mobility.psu.edu>
2016-11-28 15:46:20 -0500

    Instructions View

--- Final10/src/InstructionsView.java
+++ Final10/src/InstructionsView.java
@@ -1,3 +1,7 @@
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +12,14 @@
  *
  * @author ceciliaknaub
  */
-public class InstructionsView {
+public class InstructionsView extends JPanel{
+    private JLabel instructionsLabel;
     
+    InstructionsModel inst_model;
+    
+    InstructionsView(InstructionsModel inst_model){
+        this.inst_model = inst_model;
+        instructionsLabel = new JLabel("How to use: ");
+        
+    }
 }
