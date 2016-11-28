7861e7b5af60c5af73972d693ff1842abf3b2f25
Rasidha1121 <rahman.rasidha@gmail.com>
2016-11-28 15:45:45 -0500

    rasidha rahman

--- Final10/src/CreditView.java
+++ Final10/src/CreditView.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JLabel;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,4 +13,20 @@
  */
 public class CreditView {
     
+    JLabel developer1;
+    JLabel developer2;
+    JLabel developer3;
+    NavViewPanel nVpanel;
+    
+    CreditModel cc_model;
+    CreditView cc_view;
+    
+    CreditView(CreditModel cc_model){
+        this.cc_model = cc_model;
+        nVpanel = new NavViewPanel();
+        
+        developer1 = new JLabel("Dana Shalit");
+        developer2 = new JLabel("Rasidha Rahman");
+        developer3 = new JLabel("Cecelia Knaub");
+    }
 }
