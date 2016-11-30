b0113f41159ae0788e23397796ad491fc1219e56
Rasidha1121 <rahman.rasidha@gmail.com>
2016-11-30 15:08:46 -0500

    rasidha rahman

--- Final10/src/MainView.java
+++ Final10/src/MainView.java
@@ -29,42 +29,5 @@ public class MainView extends JPanel {
         nVpanel.removeSplash();
         nVpanel.removeMenu();
     }
-   /* public void switchToMainPanel(MainView m_view){
-        nVpanel.removeSplash();
-        nVpanel.addMain(m_view);        
-    } 
-    public void addMainButtonListener(ActionListener al) {    
-        nVpanel.removeSplash();
-        nVpanel.menu.mainButton.addActionListener(al);
-    } 
-    public void switchToCDPanel(CDView c_view){
-        nVpanel.removeSplash();
-        nVpanel.addCD(c_view); 
-    }      
-    public void addCDButtonListener(ActionListener al){
-        nVpanel.menu.applyButton.addActionListener(al);
-    }
-
-   public void switchToCreditPanel(CreditView cr_view) {
-        nVpanel.removeSplash();
-        nVpanel.addCredit(cr_view);
-    }
-    public void addCreditButtonListener(ActionListener al){
-        nVpanel.menu.creditButton.addActionListener(al);
-    }
-
-    public void switchToInstructionPanel(InstructionsView in_view){
-        nVpanel.removeSplash();
-        nVpanel.addInstructions(in_view);
-    }
-    public void addInstructionsButtonListener(ActionListener al){
-        nVpanel.menu.instructionsButton.addActionListener(al);
-    }
-    public void switchToOptionPanel(OptionView o_view) {
-        nVpanel.removeSplash();
-        nVpanel.addOption(o_view);
-    }
-    public void addOptionButtonListener(ActionListener al){
-        nVpanel.menu.optionButton.addActionListener(al);
-    }   */
+   
 }
